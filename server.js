bfb56eb6c030376df2d98d8dfef11e4d9ddf3134
ziyanh442 <ziyanh42@gmail.com>
2023-10-27 20:45:36 +0700

    setup vercel

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.listen(port, () => {console.log(`app listening on port ${port}`)})
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
+
+app.listen(port, () => {console.log(`app listening on port ${port}`)});
+
+module.exports = app;
