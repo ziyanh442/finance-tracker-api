b4f80044ec69d25665d113e45612c9d5f5d57c49
ziyanh442 <ziyanh42@gmail.com>
2023-10-27 21:15:46 +0700

    chore: change message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Express on Vercel - test automation jira");
+    res.send("Express on Vercel - test automation jira 2");
 });
 
 app.listen(port, () => {console.log(`app listening on port ${port}`)});
